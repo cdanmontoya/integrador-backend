b115506f68d209493f1ebfeb7d26ac998a595346
cdanmontoya <carlos.danmontoya@gmail.com>
2019-07-20 14:33:12 -0500

    Feature: successful DB connection

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const database = 'integrador';
+const username = 'root';
+const password = 'root';
+
+const sequelize = new Sequelize(database, username, password, {
+    host: 'database',
+    dialect: 'mysql'
+});
+
+let db = {};
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
